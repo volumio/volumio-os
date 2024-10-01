5645c4f1a1e964a6b9a1d8edfe99c801067b1a1a
ashthespy <ashthespy@users.noreply.github.com>
2024-10-01 16:28:53 +0200

    Move to Node v20
    Modules are still missing - needs to be built!

--- recipes/configurations/config.sh
+++ recipes/configurations/config.sh
@@ -49,8 +49,7 @@ VOL_BE_REPO_BRANCH="master"
 ## NodeJS Controls
 # Semver is only used w.t.r modules fetched from repo,
 # actual node version installs only respects the current major versions (Major.x)
-# NODE_VERSION=14
-NODE_VERSION=14.15.4
+NODE_VERSION=20
 # Used to pull the right version of modules
 # expected format node_modules_{arm/x86}-v${NODE_VERSION}.tar.gz
 NODE_MODULES_REPO="https://github.com/volumio/volumio3-os-static-assets/raw/master/node_modules"
