d59995dcc9199a59b3ed5fa9fd9474f277768718
foonerd <andrew@dt-ltd.net>
2024-10-01 09:06:06 +0100

    Update cm4 recipe to bookworm repo

--- recipes/devices/cm4.sh
+++ recipes/devices/cm4.sh
@@ -115,9 +115,9 @@ device_image_tweaks() {
 
 	log "Adding archive.raspberrypi debian repo" "info"
 	cat <<-EOF >"${ROOTFSMNT}/etc/apt/sources.list.d/raspi.list"
-		deb http://archive.raspberrypi.org/debian/ buster main ui
+		deb http://archive.raspberrypi.org/debian/ bookworm main
 		# Uncomment line below then 'apt-get update' to enable 'apt-get source'
-		#deb-src http://archive.raspberrypi.org/debian/ buster main ui
+		#deb-src http://archive.raspberrypi.org/debian/ bookworm main
 	EOF
 
 	# raspberrypi-{kernel,bootloader} packages update kernel & firmware files
