8995d7ff4165595220af81c57d52197f8d9da280
foonerd <andrew@dt-ltd.net>
2024-10-16 08:28:44 +0100

    [raspberry] - Bumped kernel version to 6.6.51

--- recipes/devices/pi.sh
+++ recipes/devices/pi.sh
@@ -71,9 +71,10 @@ declare -A PI_KERNELS=(
 	[6.1.77]="5fc4f643d2e9c5aa972828705a902d184527ae3f|master|1730"
 	[6.6.30]="3b768c3f4d2b9a275fafdb53978f126d7ad72a1a|master|1763"
 	[6.6.47]="a0d314ac077cda7cbacee1850e84a57af9919f94|master|1792"
+	[6.6.51]="d5a7dbe77b71974b9abb133a4b5210a8070c9284|master|1796"
 )
 # Version we want
-KERNEL_VERSION="6.6.47"
+KERNEL_VERSION="6.6.51"
 
 ### Device customisation
 # Copy the device specific files (Image/DTS/etc..)
