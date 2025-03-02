acfd02612b812d034469fba29663796f7f87bec4
foonerd <andrew@dt-ltd.net>
2025-03-02 16:07:11 +0000

    [fs_protection] - FIX: swap values in file.

--- volumio/bin/volumio_fs_protection.sh
+++ volumio/bin/volumio_fs_protection.sh
@@ -9,18 +9,18 @@ ACTION=$1
 case "$ACTION" in
     enable)
         echo "Enabling fs.protected_regular and fs.protected_fifos..."
-        sudo sysctl fs.protected_regular=1
+        sudo sysctl fs.protected_regular=2
         sudo sysctl fs.protected_fifos=1
-        echo "fs.protected_regular=1" | sudo tee /etc/sysctl.d/99-enable-protected-regular.conf
-        echo "fs.protected_fifos=1" | sudo tee /etc/sysctl.d/99-enable-protected-fifos.conf
+        echo "fs.protected_regular=1" | sudo tee /etc/sysctl.d/99-protected-regular.conf
+        echo "fs.protected_fifos=1" | sudo tee /etc/sysctl.d/99-protected-fifos.conf
         sudo sysctl --system
         ;;
     disable)
         echo "Disabling fs.protected_regular and fs.protected_fifos..."
         sudo sysctl fs.protected_regular=0
         sudo sysctl fs.protected_fifos=0
-        echo "fs.protected_regular=0" | sudo tee /etc/sysctl.d/99-disable-protected-regular.conf
-        echo "fs.protected_fifos=0" | sudo tee /etc/sysctl.d/99-disable-protected-fifos.conf
+        echo "fs.protected_regular=0" | sudo tee /etc/sysctl.d/99-protected-regular.conf
+        echo "fs.protected_fifos=0" | sudo tee /etc/sysctl.d/99-protected-fifos.conf
         sudo sysctl --system
         ;;
     status)
